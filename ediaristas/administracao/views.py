96f845206626c3ed2282fea7354d03633ed231cc
Tiago <tiagosantos0412@gmail.com>
2024-03-12 16:28:24 -0300

    Persistindo os dados do formulário no banco de dados.

--- ediaristas/administracao/views.py
+++ ediaristas/administracao/views.py
@@ -4,5 +4,10 @@ from .forms import ServicoForm
 # Create your views here.
 
 def cadastrar_servico(request):
-    form_servico = ServicoForm()
+    if request.method == 'POST':
+        form_servico = ServicoForm(request.POST)
+        if form_servico.is_valid():
+            form_servico.save()
+    else:
+        form_servico = ServicoForm()
     return render (request, 'servicos/form_servico.html', {"form_servico": form_servico})
